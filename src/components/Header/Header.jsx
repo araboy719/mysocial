4341c3f1250a80e898544016535d31381a345b7a
araboy719 <araboy719@gmail.com>
2020-11-05 00:43:37 +0300

    Some interesting style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header} >
-            <img src='https://placeit-assets1.s3-accelerate.amazonaws.com/custom-pages/fitness-logo-maker/fitness-logo-template-with-an-abstract-energetic-human-illustration-2457a-1024x1024.png'>
+            <img src='https://cdn4.iconfinder.com/data/icons/essential-app-1/16/moon-night-dard-stars-256.png'>
             </img>
         </header>
     )
