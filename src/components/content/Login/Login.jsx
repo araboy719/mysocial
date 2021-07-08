143d257a6d6ad316a15e204c1ee2450ea4df7969
araboy719 <araboy719@gmail.com>
2021-07-08 23:49:35 +0300

    75: login

--- src/components/content/Login/Login.jsx
+++ src/components/content/Login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 import s from './Login.module.css';
 
 
@@ -6,9 +7,20 @@ const Login = (props) => {
     return (
         <div className={s.content}>
             <h1>Login</h1>
+<LoginForm />
         </div >
 
     )
 }
+const LoginForm = () => {
+    return (
+        <form >
+            <input/>
+        </form>
+    )
+}
 
-export default Login;
+export default reduxForm({
+
+    form: 'login'
+})(Login)
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,13 +3,15 @@ import profileReducer from "./Profile-Reducer";
 import messageReducer from "./Message-Reducer";
 import usersReducer from "./Users-Reducer";
 import authReducer from "./auth";
-import thunkMiddleware from "redux-thunk"
+import thunkMiddleware from "redux-thunk";
+import { reducer as formReducer } from 'redux-form';
 
 let reducers = combineReducers({
     PageMessage : messageReducer,
     PageProfile : profileReducer,
     PageUsers : usersReducer,
     authData : authReducer,
+    form: formReducer,
 })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleware));
