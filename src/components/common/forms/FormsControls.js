8c0e02dbebbd54a39a829c2e6dfe9547f3b6675a
araboy719 <araboy719@gmail.com>
2021-07-15 23:56:56 +0300

    78: validate

---
+++ src/components/common/forms/FormsControls.js
@@ -0,0 +1,12 @@
+import React from "react"
+import style from './FormStyle.module.css'
+
+export const Textarea = ({input, meta, ...props}) => {
+    return (
+        <div className={style.formControll +" "+ style.error}>
+            <textarea {...props} {...input} />
+            
+            { meta.touched && meta.error && <p>some console.error();</p>}
+        </div>
+    )
+}
--- src/components/content/Profile/PostForm.jsx
+++ src/components/content/Profile/PostForm.jsx
@@ -1,11 +1,15 @@
+import { Textarea } from 'components/common/forms/FormsControls';
+import { maxLengthCreator, requiredField } from 'halpers/validators';
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+
+const maxLength10 =  maxLengthCreator(10);
 
 const PostForm = (props) => {
     return (
         <form onSubmit= {props.handleSubmit} >
             <div>
-                <Field component="input" type="text" name="newPostBody" placeholder="Message..." />
+                <Field component={Textarea} type="text" name="newPostBody" placeholder="Message..." validate={[ requiredField, maxLength10 ]} />
             </div>
             <div>
                 <button>SEND</button>
